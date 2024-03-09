cf2f4d25c969bb6a2addbca8db0fc7076582b173
James Wilcock <jameswilcock8@gmail.com>
2024-03-08 22:58:23 -0800

    errrm yeah smooth and stuff

--- Spectrum.gd
+++ Spectrum.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 const VU_COUNT = 16
 const FREQ_MAX = 11050.0
 
@@ -8,23 +7,34 @@ const WIDTH = 400
 const HEIGHT = 100
 
 const MIN_DB = 60
+const SMOOTHING = 0.5  # Adjust this value to control the smoothing effect
 
 var spectrum
-
+var average_magnitudes: PackedFloat32Array # Initialize with zeros
 
 func _ready():
-	spectrum = AudioServer.get_bus_effect_instance(0,2)
+	spectrum = AudioServer.get_bus_effect_instance(0, 2)
+	average_magnitudes.resize(VU_COUNT)
+	average_magnitudes.fill(0.0)
 
-func _process(_delta):
+func _process(delta):
 	queue_redraw()
-	
+	update_average_magnitudes()
+
+func update_average_magnitudes():
+	var w = WIDTH / VU_COUNT
+	var prev_hz = 0.0
+	for i in range(1, VU_COUNT + 1):
+		var hz = i * FREQ_MAX / VU_COUNT
+		var magnitude: float = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
+		# Update the rolling average
+		average_magnitudes[i - 1] = lerp(average_magnitudes[i - 1], magnitude, SMOOTHING)
+		prev_hz = hz
+
 func _draw():
 	var w = WIDTH / VU_COUNT
-	var prev_hz = 0
-	for i in range(1, VU_COUNT+1):
-		var hz = i * FREQ_MAX / VU_COUNT;
-		var magnitude: float = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
+	for i in range(VU_COUNT):
+		var magnitude = average_magnitudes[i]
 		var energy = clamp((MIN_DB + linear_to_db(magnitude)) / MIN_DB, 0, 1)
 		var height = energy * HEIGHT
-		draw_rect(Rect2(w * i, HEIGHT - height, w, height), Color.WHITE)
-		prev_hz = hz
+		draw_rect(Rect2(w * (i + 1), HEIGHT - height, w, height), Color.WHITE)
